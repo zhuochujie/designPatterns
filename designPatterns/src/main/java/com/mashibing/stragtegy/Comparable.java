94d87222d4beb26f393f2237084591c7c3507d09
zkg <zhao1506412996@163.com>
2019-10-18 18:15:21 +0800

    提交designPatterns

---
+++ designPatterns/src/main/java/com/mashibing/stragtegy/Comparable.java
@@ -0,0 +1,5 @@
+package com.mashibing.stragtegy;
+
+public interface Comparable<T> {
+    int compareTo(T t);
+}
---
+++ designPatterns/src/main/java/com/mashibing/stragtegy/Comparator.java
@@ -0,0 +1,5 @@
+package com.mashibing.stragtegy;
+
+public interface Comparator<T> {
+    int compare(T o1, T o2);
+}
