665679607c37a90d754ba50779a434124e6c5d1f
zkg <zhao1506412996@163.com>
2020-02-06 16:37:00 +0800

    第五节 test包，关于classloader的知识          视频时间1:16:45          1.8以前 接口不能写方法实现          1.8以后 接口可以写方法实现

--- designPatterns/src/main/java/com/mashibing/stragtegy/Comparator.java
+++ designPatterns/src/main/java/com/mashibing/stragtegy/Comparator.java
@@ -2,9 +2,19 @@ package com.mashibing.stragtegy;
 
 /**
  * 比较器Comparator 仿照参考java.util Interface Comparator<T>类写的
+ * 	第五节 test包，关于classloader的知识
+         视频时间1:16:45
+         1.8以前 接口不能写方法实现
+         1.8以后 接口可以写方法实现
  * @param <T>
  */
 @FunctionalInterface
 public interface Comparator<T> {
     int compare(T o1, T o2);
+    /*default void m(){
+        System.out.println("接口中默认方法m");
+    }*/
+    default void m2(){
+        System.out.println("接口中默认方法m2");
+    }
 }
