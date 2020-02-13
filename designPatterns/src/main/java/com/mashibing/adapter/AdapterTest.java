ab966cd629bed87d82f328c0340c6a5b29f450a4
zkg <zhao1506412996@163.com>
2020-02-13 11:14:35 +0800

    常见的Adapter类反而不是Adapter模式 				WindowAdapter 				KeyAdapter

--- designPatterns/src/main/java/com/mashibing/adapter/AdapterTest.java
+++ designPatterns/src/main/java/com/mashibing/adapter/AdapterTest.java
@@ -2,11 +2,21 @@ package com.mashibing.adapter;
 
 import java.awt.*;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+/**
+ *常见的Adapter类反而不是Adapter模式
+         WindowAdapter
+         KeyAdapter
+ */
 public class AdapterTest {
     public static void main(String[] args) {
         Frame f = new Frame();
         f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                super.windowOpened(e);
+            }
         });
     }
 }
